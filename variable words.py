5e322c60a84782c100f94814a10c7044a504dc87
Dr. Essam Al-Mizjaji <esamhasan10@yahoo.com>
2020-09-22 11:56:30 +0300

    Add files via upload

---
+++ variable words.py
@@ -0,0 +1,47 @@
+
+Character_name = 'Ali'
+character_age = '20.2'
+print('my name is ' + (Character_name) +'.')
+print('the name ' + (Character_name) +' is good')
+print('I am ' + (character_age) + ' years old')
+print('Being ' + character_age + ' is not too old')
+
+print('University of Science\"and Technology\"')
+
+phrase = 'University of Science and Technology'
+
+print (len(phrase))
+print(phrase[3])
+print(phrase.index("e"))
+print(phrase.replace("University", "Academy"))
+print(phrase)
+
+my_num = 8
+print(str (my_num) + " is my favourite number")
+
+print(abs(my_num))
+print(pow(4, 2))
+print(max (8, 3))
+print(min (8, 3))
+print(round(3.2))
+print(round(3.9))
+
+from math import *
+print(floor(4.2))
+print(ceil(4.2))
+print(sqrt(81))
+
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+print('Hello ' + name +'! You\'r ' + age +'.')
+
+
+num1 = (input("Enter a number: "))
+num2 = (input("Enter another number: "))
+
+result = (float(num1) * float(num2))
+print(result)
+
+
+
+
